dfcd151d5a64fe58200b2935f6d82a2dc6cf189f
Abhishek Chaturvedi (A.C.) <achaturvedi@salesforce.com>
2018-01-26 14:19:18 -0800

    v1.0 commit - apps and packages working

---
+++ assets/install_script_pkg2.sh
@@ -0,0 +1,9 @@
+sfdx force:package:install -i 04tf4000001I1CsAAK -w 60
+
+sfdx force:package:install -i 04tf4000001I1CxAAK -w 60
+
+sfdx force:package:install -i 04tf4000001I1D2AAK -w 60
+
+sfdx force:package:install -i 04tf4000001I1D7AAK -w 60
+
+sfdx force:user:permset:assign -n dreamhouse -u newPkgTest
